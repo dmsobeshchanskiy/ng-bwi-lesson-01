4b8d21b0b8e29bf52bca1dce51e82082a1523cd1
Dmytro Sobeshchanskyy <dsob@isd.dp.ua>
2019-11-21 19:22:27 +0200

    Remove generated code. Components drafts

--- bwi-app/src/app/app.module.ts
+++ bwi-app/src/app/app.module.ts
@@ -2,11 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './gui/app.component';
+import { HistoryComponent } from './gui/history/history.component';
+import { InputComponent } from './gui/input/input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputComponent,
+    HistoryComponent
   ],
   imports: [
     BrowserModule,
