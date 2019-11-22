642b5828f5552e1c4074aad5681c01e5b769ab41
Dmytro Sobeshchanskyy <dsob@isd.dp.ua>
2019-11-22 13:45:44 +0200

    Remove magic numbers. Correct error codes.

--- bwi-app/src/app/weight-analyzer/error-codes.ts
+++ bwi-app/src/app/weight-analyzer/error-codes.ts
@@ -2,7 +2,7 @@ export class ErrorCodes {
     public static readonly WeightBelowZero = 'WeightBelowZero';
     public static readonly WeightOverflow = 'WeightOverflow';
 
-    public static readonly HeightBelowLimit = 'HeightBelowZero';
+    public static readonly HeightBelowLimit = 'HeightBelowLimit';
     public static readonly HeightOverflow = 'HeightOverflow';
 
     public static readonly AgeBelowZero = 'AgeBelowZero';
--- bwi-app/src/app/weight-analyzer/weight-analyzer.service.ts
+++ bwi-app/src/app/weight-analyzer/weight-analyzer.service.ts
@@ -4,53 +4,66 @@ import { WeightAnalyzerResponse } from './weight-analyzer-response';
 import { ErrorCodes } from './error-codes';
 
 @Injectable({
-  providedIn: 'root'
+   providedIn: 'root'
 })
 export class WeightAnalyzerService {
 
-  public analyze(input: WeightAnalyzerInput): WeightAnalyzerResponse {
-    const response = new WeightAnalyzerResponse();
-    const error = this.getValidationError(input);
-    if (error && error.length > 0) {
-      response.occuredError = error;
-    } else {
-      const perfectWeight = this.calcPerfectWeight(input);
-      response.weightDeviation = Math.round(input.weight - perfectWeight);
-      response.weightDeviationPercentage = Math.round((input.weight - perfectWeight) * 100 / perfectWeight);
-    }
+   private switchingAge = 40;
+   private belowCoefficient = 110;
+   private aboveCoefficient = 100;
 
-    return response;
-  }
+   private heightUpperLimit = 250;
+   private heightDownLimit = 110;
 
-  constructor() { }
+   private weightUpperLimit = 500;
+   private weightDownLimit = 0;
 
-  private getValidationError(input: WeightAnalyzerInput): string {
-    let response = '';
-    if (input.height >= 250) {
-      response = ErrorCodes.HeightOverflow;
-    } else if (input.height < 110) {
-      response = ErrorCodes.HeightBelowLimit;
-    } else if (input.weight >= 500) {
-      response = ErrorCodes.WeightOverflow;
-    } else if (input.weight <= 0) {
-      response = ErrorCodes.WeightBelowZero;
-    } else if (input.age >= 150) {
-      response = ErrorCodes.AgeOverflow;
-    } else if (input.age < 0) {
-      response = ErrorCodes.AgeBelowZero;
-    }
+   private ageUpperLimit = 150;
+   private ageDownLimit = 0;
 
-    return response;
-  }
+   public analyze(input: WeightAnalyzerInput): WeightAnalyzerResponse {
+      const response = new WeightAnalyzerResponse();
+      const error = this.getValidationError(input);
+      if (error && error.length > 0) {
+         response.occuredError = error;
+      } else {
+         const perfectWeight = this.calcPerfectWeight(input);
+         response.weightDeviation = Math.round(input.weight - perfectWeight);
+         response.weightDeviationPercentage = Math.round((input.weight - perfectWeight) * 100 / perfectWeight);
+      }
 
-  private calcPerfectWeight(input: WeightAnalyzerInput): number {
-    let response = 0;
-    if (input.age <= 40) {
-      response = input.height - 110;
-    } else {
-      response = input.height - 100;
-    }
-    return response;
-  }
+      return response;
+   }
+
+   constructor() { }
+
+   private getValidationError(input: WeightAnalyzerInput): string {
+      let response = '';
+      if (input.height >= this.heightUpperLimit) {
+         response = ErrorCodes.HeightOverflow;
+      } else if (input.height < this.heightDownLimit) {
+         response = ErrorCodes.HeightBelowLimit;
+      } else if (input.weight >= this.weightUpperLimit) {
+         response = ErrorCodes.WeightOverflow;
+      } else if (input.weight <= this.weightDownLimit) {
+         response = ErrorCodes.WeightBelowZero;
+      } else if (input.age >= this.ageUpperLimit) {
+         response = ErrorCodes.AgeOverflow;
+      } else if (input.age < this.ageDownLimit) {
+         response = ErrorCodes.AgeBelowZero;
+      }
+
+      return response;
+   }
+
+   private calcPerfectWeight(input: WeightAnalyzerInput): number {
+      let response = 0;
+      if (input.age <= this.switchingAge) {
+         response = input.height - this.belowCoefficient;
+      } else {
+         response = input.height - this.aboveCoefficient;
+      }
+      return response;
+   }
 
 }
